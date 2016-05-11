3d32d5d3d753bee7f9c392cfc87f761ce7db0eac
hallamager <HallamAger@Hallams-MacBook-Pro.local>
2016-05-11 15:30:40 +0100

    added launch screen

--- Folding/ViewController.swift
+++ Folding/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
-    let kCloseCellHeight: CGFloat = 159
+    let kCloseCellHeight: CGFloat = 160
     let kOpenCellHeight: CGFloat = 470
     
     var routes = buildRoutes()
