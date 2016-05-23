80958bfc78cf80c4e5311fa9b12e802132fe2941
Olly Boon <ollieboon1@gmail.com>
2016-05-22 18:03:48 -0700

    Changed path info

--- Folding/Route.swift
+++ Folding/Route.swift
@@ -42,56 +42,56 @@ func buildRoutes() -> [Route] {
 
     routes.append(Route(name: "Sensory Path",
         foldingName: "Sensory Path",
-        duration: "15 MINS",
-        info: "Take a journey through the estate's varied environments",
+        duration: "15 Mins",
+        info: "Stimulating the senses, taking you through a variety of habitats",
         terrainInfo: "Variable path surface, some grassed areas",
         terrainImage: "terrainImage1",
         mapRouteImage: "mapRouteImage1",
-        color: UIColor(redX: 255, greenX: 181, blueX: 90, alphaX: 1),
+        color: UIColor(redX: 96, greenX: 142, blueX: 230, alphaX: 1),
         colorDiff: "green"
         )
     )
     routes.append(Route(name: "Bird Hide Path",
         foldingName: "Bird Hide Path",
-        duration: "5 MINS",
-        info: "Take a journey through the estate's varied environments",
+        duration: "5 Mins",
+        info: "Take a journey to the Bird hide, an ideal place to view the varied bird species of Holton Lee",
         terrainInfo: "Variable path surface, some wet areas",
         terrainImage: "terrainImage1",
         mapRouteImage: "mapRouteImage2",
-        color: UIColor(redX: 225, greenX: 229, blueX: 90, alphaX: 1),
+        color: UIColor(redX: 189, greenX: 140, blueX: 72, alphaX: 1),
         colorDiff: "green"
         )
     )
     routes.append(Route(name: "The Clump Path",
         foldingName: "The Clump Path",
-        duration: "10 MINS",
-        info: "Take a journey through the estate's varied environments",
+        duration: "10 Mins",
+        info: "Enjoy a walk to the Clump, with stunning views and our impressive weaving tree",
         terrainInfo: "Variable path surface, some dry areas",
         terrainImage: "terrainImage1",
         mapRouteImage: "mapRouteImage1",
-        color: UIColor(redX: 90, greenX: 193, blueX: 242, alphaX: 1),
+        color: UIColor(redX: 225, greenX: 110, blueX: 110, alphaX: 1),
         colorDiff: "orange"
         )
     )
-    routes.append(Route(name: "The Clump Fields",
-        foldingName: "The Clump Fields",
-        duration: "15 MINS",
-        info: "Take a journey through the estate's varied environments",
+    routes.append(Route(name: "Sea View Path",
+        foldingName: "Sea View Path",
+        duration: "15 Mins",
+        info: "A trail leading you to the sea, with a great vantage point",
         terrainInfo: "Variable path surface, some muddy areas",
         terrainImage: "terrainImage1",
         mapRouteImage: "mapRouteImage1",
-        color: UIColor(redX: 171, greenX: 53, blueX: 204, alphaX: 1),
+        color: UIColor(redX: 216, greenX: 132, blueX: 253, alphaX: 1),
         colorDiff: "orange"
         )
     )
     routes.append(Route(name: "Heathland Path",
         foldingName: "Heathland Path",
-        duration: "25 MINS",
+        duration: "25 Mins",
         info: "Take a journey through the estate's varied environments",
         terrainInfo: "Variable path surface, some epic areas",
         terrainImage: "terrainImage1",
         mapRouteImage: "mapRouteImage1",
-        color: UIColor(redX: 242, greenX: 12, blueX: 43, alphaX: 1),
+        color: UIColor(redX: 241, greenX: 95, blueX: 140, alphaX: 1),
         colorDiff: "red"
         )
     )
